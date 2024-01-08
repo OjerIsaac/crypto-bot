d7488c2cc11ad7fa2df57f913effde85228591d8
OjerIsaac <isaacojerumu@gmail.com>
2024-01-08 09:04:33 +0100

    update3

--- src/modules/bot/bot.service.ts
+++ src/modules/bot/bot.service.ts
@@ -23,7 +23,7 @@ export class BotService {
     }
 
     private async loginWithCredentials({ email, password }: Credentials): Promise<void> {
-        const browser = await puppeteer.launch({ headless: false });
+        const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
 
         try {
